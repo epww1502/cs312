bc2bcbce198a7277193110ea9d8ab97603da3df1
epww1502 <43550999+epww1502@users.noreply.github.com>
2020-05-18 01:23:56 -0700

    Create hw7script.sh

---
+++ Homework7_YoungjooLee/hw7script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -e ~/.ssh/id_rsa.pub ]
+then
+	echo -e "ssh key alreaday existed."
+else
+	echo -e "ssh key is generating..."
+	ssh-keygen -t rsa 
+	$not_my_first_time=1
+fi
+
+echo -e "Copy the SSH key"
+ssh-copy-id root@192.168.1.20
+ssh-copy-id root@192.168.1.21
+ssh-copy-id root@192.168.1.22
+ssh-copy-id root@192.168.1.23
+
+ansible-playbook webserver.yaml -i hosts.ini
+
+echo -e "Return contents of index.html"
+curl 192.168.1.20
+curl 192.168.1.21
+curl 192.168.1.22
+curl 192.168.1.23
+
